1c3fdf43d2fe6679c5f676ea0a780c998094cfaa
gin134rus <92790789+gin134rus@users.noreply.github.com>
2024-09-25 15:38:58 +0300

    Добавьте файлы проекта.

---
+++ 4.4.1/4.4.1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+void main() {
+
+    setlocale(LC_ALL, "russian");
+
+    int number1;
+    int number2;
+    std::cout << "Введите первое число:\n";
+    std::cin >> number1;
+    std::cout << "Введите второе число: \n";
+    std::cin >> number2;
+    std::cout << "---проверяем----\n";
+    if (number1 > number2) {
+        std::cout << "наименьшее чесло: " << number2;
+    }
+    else if (number1 < number2) {
+        std::cout << "наименьшее чесло: " << number1;
+    }
+    else {
+        std::cout << "числа равны: " << number1;
+    }
+}
